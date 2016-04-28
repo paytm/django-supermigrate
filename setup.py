d38324a0c6e02507d6f36ede167bfb1ece7c8364
Taranjeet <reachtotj@gmail.com>
2016-04-28 15:57:33 +0530

    Setup: Update with find_packages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 setup(
     name='django-supermigrate',
     version='0.1',
-    packages=['supermigrate'],
+    packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     description='package to manage migrations on production and development environment simultaneously',
