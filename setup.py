30cacdcc798fbfd2c0b371e2f630c9324375dcce
Taranjeet <reachtotj@gmail.com>
2016-04-28 16:06:26 +0530

    Setup: Version bump to 0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 
 setup(
     name='django-supermigrate',
-    version='0.1',
+    version='0.3',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
